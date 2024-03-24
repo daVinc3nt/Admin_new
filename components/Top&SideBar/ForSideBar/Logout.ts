da6d7d13da25976327a2af24a4ca36f1894c263c
QuangTran013 <113117168+daVinc3nt@users.noreply.github.com>
2024-03-24 12:48:32 +0700

    fixed logoutout

--- components/Top&SideBar/ForSideBar/Logout.ts
+++ components/Top&SideBar/ForSideBar/Logout.ts
@@ -1,8 +1,9 @@
 import { StaffsOperation } from "@/TDLib/tdlogistics";
 import Cookies from "js-cookie"
+import { useRouter } from "next/router";
 const action = new StaffsOperation()
 export function Logout(){
-
+    const router =useRouter()
     const confirmDelete = () => {
         return window.confirm("Bạn có muốn là thoát phiên đăng nhập không?");
     };
@@ -11,8 +12,10 @@ export function Logout(){
     const result = confirmDelete();
     // Nếu result là true, tức là người dùng nhấn yes
     if (result) {
+
         Cookies.remove("connect.sid");
         action.logout()
+        router.push("/")
     }
     // Nếu result là false, tức là người dùng nhấn no
     else {
