d30f0a24baf138ef56e40ff889b0c973c911e90b
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2024-04-07 22:09:08 +0700

    fix bug

--- api/getConsignment.tsx
+++ api/getConsignment.tsx
@@ -1,34 +1,45 @@
 import { ShipmentsOperation } from "@/TDLib/tdlogistics";
-import { columns } from "../components/Consignment/Table/TableInfo/column";
+import { createColumns } from "../components/Consignment/Table/TableInfo/column";
 import { DataTable } from "../components/Consignment/Table/TableInfo/datatable";
 
 async function getData() {
-    try {
-        const shipments = new ShipmentsOperation();
-        const data = await shipments.get({});
-        return data;
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        throw error;
-    }
+  try {
+    const shipments = new ShipmentsOperation();
+    const data = await shipments.get({});
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 }
 
-export default async function DemoPage(reloadData, setOpenError, setMessage) {
-    try {
-        const data = await getData();
-        if (!data.error) return <DataTable columns={columns} data={data.data} reloadData={reloadData} />;
-        else {
-            setMessage(data.message);
-            setOpenError(true)
-            return (
-                <div className="flex place-content-center h-screen justify-center place-items-center">
-                    "Error! Please contact admin."
-                </div>
-            );
-        }
-    } catch (error) {
-        return <div className="flex place-content-center h-screen justify-center place-items-center">
-            "Error! Please contact admin."
+export default async function DemoPage(
+  reloadData,
+  setOpenError,
+  setMessage,
+  info
+) {
+  try {
+    const data = await getData();
+    const columns = await createColumns(reloadData, info);
+    if (!data.error)
+      return (
+        <DataTable columns={columns} data={data.data} reloadData={reloadData} />
+      );
+    else {
+      setMessage(data.message);
+      setOpenError(true);
+      return (
+        <div className="flex place-content-center h-screen justify-center place-items-center">
+          "Error! Please contact admin."
         </div>
+      );
     }
+  } catch (error) {
+    return (
+      <div className="flex place-content-center h-screen justify-center place-items-center">
+        "Error! Please contact admin."
+      </div>
+    );
+  }
 }
