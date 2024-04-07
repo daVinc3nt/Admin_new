9ef3440a84ecd37b857f540b5fa080b7c0ee7ecf
Ho Thanh Nha <hothanhnha25062004@gmail.com>
2024-04-07 12:02:00 +0700

    New feature approved2

---
+++ components/Consignment/approved.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Button } from "@nextui-org/react";
+import axios from "axios";
+import { ShipmentsOperation, ShipmentID } from "@/TDLib/tdlogistics";
+
+export function approve(
+  row: any,
+  info: any,
+  reloadData: () => void
+): JSX.Element {
+  if (
+    info?.role === "ADMIN" ||
+    info?.role === "MANAGER" ||
+    info?.role === "TELLER"
+  ) {
+    const a = new ShipmentsOperation();
+    // axios.get("http://localhost:5000/get_session").then((res) => {
+    //   console.log("res", res.data);
+    // });
+
+    const handleConfirm = async () => {
+      const ID: ShipmentID = {
+        shipment_id: row.original.shipment_id,
+      };
+      console.log("ID", ID);
+      try {
+        const response = await a.approve(ID);
+        console.log("response", response);
+        if (response.error) {
+          alert(response.message);
+          return;
+        }
+        alert(response.message);
+        reloadData();
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    return (
+      <div className="relative mr-2 w-full flex justify-center">
+        <Button
+          onClick={handleConfirm}
+          className="bg-transparent hover:bg-white font-bold hover:text-black py-1 px-[0.65rem] border border-gray-600 hover:border-transparent rounded-full "
+        >
+          ✔
+        </Button>
+      </div>
+    );
+  }
+}
